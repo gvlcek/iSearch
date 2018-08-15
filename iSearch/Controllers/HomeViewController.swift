af9daeea7b567655378f4eedd2c49ef856332c9f
Guadalupe Vlček <gvlcek@icloud.com>
2018-08-15 00:23:35 -0300

    Feature: added project folders and created base view controller and homeviewcontroller

---
+++ iSearch/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  iSearch
+//
+//  Created by Guadalupe Vlcek on 14/08/2018.
+//  Copyright © 2018 Guadalupe Vlcek. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
